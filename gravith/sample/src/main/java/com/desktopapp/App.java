9f4256bf151a374160e9aee6e845ad415caca59e
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-10-23 11:41:24 -0300

    javaff

---
+++ gravith/sample/src/main/java/com/desktopapp/App.java
@@ -0,0 +1,19 @@
+package com.desktopapp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+   
+    public static void main(String[] args) {
+        launch();
+    }
+
+}

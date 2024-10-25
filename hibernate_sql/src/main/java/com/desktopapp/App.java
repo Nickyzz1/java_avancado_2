818d51e8bef5e52a4135145f440b832e4c132398
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-10-25 11:34:01 -0300

    dando certo

--- hibernate_sql/src/main/java/com/desktopapp/App.java
+++ hibernate_sql/src/main/java/com/desktopapp/App.java
@@ -12,22 +12,21 @@ public class App extends Application{
         // user.setName("");
         // user.setPassword("");
 
-        // Context ctx = new Context();
+        Context ctx = new Context();
         // ctx.begin();
         // ctx.save(user);
         // ctx.commit();
 
         // Product prod1 = new Product();
         // prod1.setNameProd("Inquisicao vol 1");
-        // prod1.setValueProd(10.0);
+        // prod1.setPriceProd(10.0);
 
         // ctx.begin();
         // ctx.save(prod1);
         // ctx.commit();
 
         // Product prod2 = new Product();
-        // prod2.setNameProd("Memorias do subsolo");
-        // prod2.setValueProd(10.0);
+        // prod2.setPriceProd(10.0);
 
         // ctx.begin();
         // ctx.save(prod2);
@@ -35,7 +34,7 @@ public class App extends Application{
 
         // Product prod3 = new Product();
         // prod3.setNameProd("Inquisicao vol 2");
-        // prod3.setValueProd(10.0);
+        // prod3.setPriceProd(10.0);
 
         // ctx.begin();
         // ctx.save(prod3);
